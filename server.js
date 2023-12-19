2b2552ca70faa39039ff2ded81491bad2680e28a
bobby-216digital <95427013+bobby-216digital@users.noreply.github.com>
2023-12-19 18:51:13 -0500

    yjkjyuk

--- server.js
+++ server.js
@@ -714,7 +714,8 @@ function doFetch (query, res, check, obj) {
               res.send(data)
             }
             if (check) {
-              //let siteData = data.data.addSortsiteScan.sortsiteScan[0].site;
+              console.log(data.data.addSortsiteScan)
+              let siteData = data.data.addSortsiteScan.sortsiteScan[0].site;
               checkThresholds(siteData, [Object.keys(obj['a']).length, Object.keys(obj['aa']).length, Object.keys(obj['aaa']).length])
             }
            
