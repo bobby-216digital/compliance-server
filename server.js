947dd0261a3a592d7c278b9cff6319f858228e70
bobby-216digital <95427013+bobby-216digital@users.noreply.github.com>
2023-12-19 18:55:44 -0500

    thurtyurtu

--- server.js
+++ server.js
@@ -713,8 +713,8 @@ function doFetch (query, res, check, obj) {
             }
             if (check) {
               console.log(data.data.addSortsiteScan)
-              let siteData = data.data.addSortsiteScan.sortsiteScan[0].site;
-              checkThresholds(siteData, [Object.keys(obj['a']).length, Object.keys(obj['aa']).length, Object.keys(obj['aaa']).length])
+             // let siteData = data.data.addSortsiteScan.sortsiteScan[0].site;
+             // checkThresholds(siteData, [Object.keys(obj['a']).length, Object.keys(obj['aa']).length, Object.keys(obj['aaa']).length])
             }
            
         })
